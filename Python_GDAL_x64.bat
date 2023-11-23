2361b7b4f016555a894200a4f6e43ad7d7c3b239
MeenavaishaliR <138771153+MeenavaishaliR@users.noreply.github.com>
2023-11-23 11:46:59 +0530

    Update Python_GDAL_x64.bat

--- Python_GDAL_x64.bat
+++ Python_GDAL_x64.bat
@@ -13,11 +13,11 @@ echo .
 echo Sit back and relax while your files are being downloaded.
 echo .
 echo .
-echo Downloading Python 3.7.9 64-bit
+echo Downloading Python 3.9.18 64-bit
 echo ....................................................................................
 echo ####################################################################################
 :: If you want to download a different Python version, update the below line.
-curl --output pratyush_temporary\python-3.7.9-amd64.exe --url https://www.python.org/ftp/python/3.7.9/python-3.7.9-amd64.exe
+curl --output pratyush_temporary\python-3.9.18-amd64.exe --url https://www.python.org/ftp/python/3.9.18/python-3.9.18-amd64.exe
 
 echo .
 echo .
@@ -33,14 +33,14 @@ echo Downloading GDAL for Python
 echo ....................................................................................
 echo ####################################################################################
 :: If you changed the Python version, update the below line too.
-curl --output pratyush_temporary\GDAL-3.1.3.win-amd64-py3.7.msi --url http://download.gisinternals.com/sdk/downloads/release-1900-x64-gdal-3-1-3-mapserver-7-6-1/GDAL-3.1.3.win-amd64-py3.7.msi
+curl --output pratyush_temporary\GDAL-3.1.3.win-amd64-py3.9.msi --url http://download.gisinternals.com/sdk/downloads/release-1900-x64-gdal-3-1-3-mapserver-7-6-1/GDAL-3.1.3.win-amd64-py3.9.msi
 
 echo .
 echo .
 
 :: Perform checks for the files.
-if exist pratyush_temporary\python-3.7.9-amd64.exe (
-echo Python 3.7.9 64bit successfully downloaded!
+if exist pratyush_temporary\python-3.9.18-amd64.exe (
+echo Python 3.9.18 64bit successfully downloaded!
 ) else (
 echo Python download failed. The program will now terminate, please start manually.
 TIMEOUT /T 15
@@ -55,7 +55,7 @@ TIMEOUT /T 15
 exit /B
 )
 
-if exist pratyush_temporary\GDAL-3.1.3.win-amd64-py3.7.msi (
+if exist pratyush_temporary\GDAL-3.1.3.win-amd64-py3.9.msi (
 echo GDAL for Python successfully downloaded!
 ) else (
 echo GDAL for Python download failed. The program will now terminate, please start manually.
@@ -69,9 +69,9 @@ echo Proceeding to installation. Please assign the installation directories..
 TIMEOUT /T 10
 
 :: Run the three setup files successively.
-pratyush_temporary\python-3.7.9-amd64.exe
+pratyush_temporary\python-3.9.18-amd64.exe
 pratyush_temporary\gdal-301-1900-x64-core.msi
-pratyush_temporary\GDAL-3.1.3.win-amd64-py3.7.msi
+pratyush_temporary\GDAL-3.1.3.win-amd64-py3.9.msi
 
 echo .
 echo .
@@ -79,8 +79,8 @@ echo All programs installed. Adding paths to environment variables.
 
 :: Add the path to system environment variables.
 setx /m path "%path%;C:\Program Files\GDAL;
-setx /m path "%path%;C:\Python37;
-setx /m path "%path%;C:\Python37\Scripts;
+setx /m path "%path%;C:\Python39;
+setx /m path "%path%;C:\Python39\Scripts;
 
 :: Create new variables in the system environment for GDAL.
 setx /m GDAL_DATA "C:\Program Files\GDAL\gdal-data"
@@ -88,7 +88,7 @@ setx /m GDAL_DRIVER_PATH "C:\Program Files\GDAL\gdalplugins"
 setx /m GDAL_VERSION "3.1.1"
 
 :: Rename the Python file to be able to call from Command Prompt.
-copy "C:\Python37\python.exe" "C:\Python37\python37.exe"
+copy "C:\Python39\python.exe" "C:\Python39\python39.exe"
 
 echo .
 echo .
